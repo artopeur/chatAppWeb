717f30dfd771ad7eeecd7bf9eb9ad40c51a3e0d9
artopeur <arto.peurasaari@gmail.com>
2026-02-11 01:46:57 +0200

    backend start.

---
+++ backend/routes/webApp.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+var msgStorage = [];
+
+
+var webApp = (
+  router.get('/', function(req, res, next) {
+    console.log(req);
+    res.json({message: "Hello World"});
+    console.log("Main route hit");
+  },
+  router.post('/message', function(req, res, next) {
+    console.log(req.body);
+    msgStorage.push(req.body);
+    res.json({message: "Message received"});
+    console.log("Message route hit");
+  }),
+  router.get('/messages', function(req, res, next) {
+    res.json(msgStorage);
+    console.log("Messages route hit");
+  })
+  
+));
+
+module.exports = webApp;
