ff510c5f56dab87b71e5b289e96a1595abf3ae8d
artopeur <arto.peurasaari@gmail.com>
2026-02-19 16:02:30 +0200

    bugfix.

--- backend/sockets/chatSocket.js
+++ backend/sockets/chatSocket.js
@@ -36,7 +36,7 @@ function initChatSocket(server) {
 
       // store message in history
       messageStore.add(roomId, message);
-      let sql = `INSERT INTO chats(sender, message) VALUES(${socket.username}, ${socket.message.text}`;
+      let sql = `INSERT INTO chats(sender, message) VALUES(${socket.username}, ${socket.message}`;
       database.query(sql, function(error, response) {
         if(error) {
           console.log(error);
